ea9ad1025a0216826e85b84364b3f6eb3762d97f
pandarudra <rudrapanda8206@gmail.com>
2024-12-06 00:10:23 +0530

    .

--- webRTC/peerService.js
+++ webRTC/peerService.js
@@ -1,6 +1,8 @@
 import { PeerServer } from "peer";
+import dotenv from "dotenv";
+dotenv.config();
 
-const peerServer = PeerServer({ port: 9000, path: "/peerjs" });
+const peerServer = PeerServer({ port: process.env.PORT, path: "/peerjs" });
 
 peerServer.on("connection", (client) => {
   console.log("Client connected:", client.id);
